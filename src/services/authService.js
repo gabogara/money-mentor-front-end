3d6bd7426e828b701d3006f9561944643c140bf1
suzanaai <angelska327@gmail.com>
2026-01-21 16:20:01 -0600

    small parenthesis error in the error section

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,48 +1,49 @@
-const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/auth`
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/auth`;
 
 const signUp = async (formData) => {
-    try {
-        const res = await fetch(`${BASE_URL}/sign-up`, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(formData),
-        })
-        const data = await res.json()
-        if (data.err) {
-            throw new Error(data.err)
-        }
-        if (data.token) {
-            localStorage.setItem('token', data.token)
+  try {
+    const res = await fetch(`${BASE_URL}/sign-up`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(formData),
+    });
+    const data = await res.json();
+    if (data.err) {
+      throw new Error(data.err);
+    }
+    if (data.token) {
+      localStorage.setItem("token", data.token);
 
-            return JSON.parse(atob(data.token.split('.')[1])).payload
-        }
-        throw new Error('Invalid response from server')
-    } catch (err) {}
-    console.log(err)
-    throw new Error(err)
-}
+      return JSON.parse(atob(data.token.split(".")[1])).payload;
+    }
+    throw new Error("Invalid response from server");
+  } catch (err) {
+    console.log(err);
+    throw new Error(err);
+  }
+};
 
 const signIn = async (formData) => {
-    try {
-        const res = await fetch(`${BASE_URL}/sign-in`, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(formData),
-        })
+  try {
+    const res = await fetch(`${BASE_URL}/sign-in`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(formData),
+    });
 
-        const data = await res.json()
+    const data = await res.json();
 
-        if (data.err) {
-            throw new Error(data.err)
-        }
-        if (data.token) {
-            localStorage.setItem('token', data.token)
-            return JSON.parse(atob(data.token.split('.')[1])).payload
-        }
-        throw new Error('Invalid response from server')
-    } catch (err) {
-        console.log(err)
-        throw new Error(err)
+    if (data.err) {
+      throw new Error(data.err);
     }
-}
-export { signUp, signIn }
+    if (data.token) {
+      localStorage.setItem("token", data.token);
+      return JSON.parse(atob(data.token.split(".")[1])).payload;
+    }
+    throw new Error("Invalid response from server");
+  } catch (err) {
+    console.log(err);
+    throw new Error(err);
+  }
+};
+export { signUp, signIn };
